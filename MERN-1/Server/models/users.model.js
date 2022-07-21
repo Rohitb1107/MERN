1703a12f460b615e9dc4d351e28be59ee8dbdc49
Rohit <stylishrb711@gmail.com>
2022-07-21 16:07:49 +0530

    controllers added

---
+++ MERN-1/Server/models/users.model.js
@@ -0,0 +1,30 @@
+const mongoose = require("mongoose");
+
+// create schema
+const userSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true,
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  password: {
+    type: String,
+    required: true,
+  },
+  cpassword: {
+    type: String,
+    required: true,
+  },
+});
+
+// create model/collections
+const User = new mongoose.model("user", userSchema);
+
+// create documents
+
+// export
+module.exports = User;
